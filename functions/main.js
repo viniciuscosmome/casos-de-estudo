10f37a47c76b43a4a3d8b416844d936734aa19ae
Vinicius Cosmo <viniciuscosmosp@gmail.com>
2022-01-05 03:16:14 -0300

    adicionando botão para voltar a home

--- functions/main.js
+++ functions/main.js
@@ -1,10 +1,10 @@
 let getAllSelectButtons = document.querySelectorAll('.select')
+let getDisplayPreview = document.querySelector('[data-preview]')
+let getIframe = document.querySelector('.iframe')
+let getEntryButton = document.querySelector('.entry')
 
 function changePreview(el) {
     let reference = el.getAttribute('data-href')
-    let getDisplayPreview = document.querySelector('[data-preview]')
-    let getIframe = document.querySelector('.iframe')
-    let getEntryButton = document.querySelector('.entry')
 
     getDisplayPreview.setAttribute('data-preview', `DESAFIO ${reference}`)
     getIframe.setAttribute('src', `desafios/desafio_${reference}`)
