480b5e2a719ff002653c7ea797bac0a60b8bfe94
Vinicius Cosmo <viniciuscosmosp@gmail.com>
2022-01-06 22:00:47 -0300

    editando a tela de preview

--- functions/main.js
+++ functions/main.js
@@ -1,22 +1,28 @@
 let getAllSelectButtons = document.querySelectorAll('.select')
-let getDisplayPreview = document.querySelector('[data-preview]')
 let getIframe = document.querySelector('.iframe')
 let getEntryButton = document.querySelector('.entry')
 
-function changePreview(el) {
-    let reference = el.getAttribute('data-href')
+function changePreview() {
+    let reference = this.getAttribute('data-href')
 
-    getDisplayPreview.setAttribute('data-preview', `DESAFIO ${reference}`)
     getIframe.setAttribute('src', `desafios/desafio_${reference}`)
     getEntryButton.setAttribute('href', `desafios/desafio_${reference}`)
+
+    changeButtonActive(this)
+}
+
+function changeButtonActive(currentButton) {
+    getAllSelectButtons.forEach(function (el) {
+        el.classList.remove('active')
+    })
+
+    currentButton.classList.add('active')
 }
 
 if (!!getAllSelectButtons && getAllSelectButtons.length > 0) {
 
     getAllSelectButtons.forEach(function(element) {
-        element.addEventListener('click', function() {
-            changePreview(element)
-        })
+        element.addEventListener('click', changePreview)
     })
 
 }
