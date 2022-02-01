394e8b772bccc946b1a10c8f6463c3c38f739284
Vinicius Cosmo <viniciuscosmosp@gmail.com>
2022-02-01 05:45:41 -0300

    removendo jogo da memoria

--- desafios/iuricode/desafio_11/styles/js/main.js
+++
@@ -1,9 +0,0 @@
-const initStyle = function() {
-    let selectList = document.querySelector('.select-list')
-
-    selectList.addEventListener('click', function() {
-        selectList.classList.toggle('show')
-    })
-}
-
-window.addEventListener('load', initStyle)
